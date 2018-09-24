73d0dcfe80341f79f95c7f65b5780f08989a6f40
alltay <alltay@ya.ru>
2018-09-24 22:32:21 +0300

    first commit

---
+++ ad-widget.js
@@ -0,0 +1,32 @@
+var cat_id = $("#ad_sub_cat_widget").attr("cat-id")
+document.getElementById("ad_sub_cat_widget").style.display = "none"; 
+
+
+function getSlider(cat_id){
+var request = new XMLHttpRequest();
+    function reqReadyStateChange() {
+        if (request.readyState == 4) {
+            var status = request.status;
+            if (status == 200) {
+                document.getElementById("ad_sub_cat_widget").innerHTML=request.responseText;
+            }
+        }
+    }
+    var body = cat_id;
+    request.open("GET", "http://localhost:8000/?"+body); // Ned to change on relise
+    request.onreadystatechange = reqReadyStateChange;
+    request.send();
+}
+
+$.getScript("https://alltay.github.io/blocks/jquery-migrate-1.2.1.min.js");
+$.getScript("https://alltay.github.io/blocks/slick.min.js"); // Need to change on relese
+getSlider(cat_id)
+
+setTimeout(function(){
+    $.getScript("https://alltay.github.io/blocks/slider.js");// Need to change on relese
+}, 2000);
+
+setTimeout(function(){
+    document.getElementById("ad_sub_cat_widget").style.display = "block"; 
+}, 2000);
+
