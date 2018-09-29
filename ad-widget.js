4d1a6abc639f98b8de96c612f02834a42986c585
alltay <alltay@ya.ru>
2018-09-30 01:29:43 +0300

    test update

--- ad-widget.js
+++ ad-widget.js
@@ -1,34 +1,48 @@
-var cat_id = $("#ad_sub_cat_widget").attr("cat-id")
-var site_name = $("#ad_sub_cat_widget").attr("site-name")
-document.getElementById("ad_sub_cat_widget").style.display = "none"; 
+// Check if widget block exists in sub category
+if(document.getElementById('ad_sub_cat_widget')){
+    var widget = 'ad_sub_cat_widget';
+}
+// Check if widget block exists in main category
+if(document.getElementById('ad_cat_widget')){
+    var widget = 'ad_cat_widget';
+}
+// Check it widget exists on item page
+if(document.getElementById('ad_item_widget')){
+    var widget = 'ad_item_widget';
+}
 
+// Get widget params
+var cat_id = $("#" + widget).attr("cat-id");
+var site_name = $("#" + widget).attr("site-name");
+document.getElementById(widget).style.display = "none"; 
 
-function getSlider(cat_id, site_name){
+// Get widget body from back end
+function getSlider(cat_id, site_name, widget){
 var request = new XMLHttpRequest();
     function reqReadyStateChange() {
         if (request.readyState == 4) {
             var status = request.status;
             if (status == 200) {
-                document.getElementById("ad_sub_cat_widget").innerHTML=request.responseText;
+                document.getElementById(widget).innerHTML = request.responseText;
             }
         }
     }
-    var body = cat_id;
-    var aditional = site_name;
-    request.open("GET", "https://block.mirkrestikom.ru/?id="+body+"&site="+aditional); // Ned to change on relise
+    request.open("GET", "https://block.mirkrestikom.ru/?id=" + cat_id + "&site=" + site_name);
     request.onreadystatechange = reqReadyStateChange;
     request.send();
 }
 
+// Get js libs
 $.getScript("https://alltay.github.io/blocks/jquery-migrate-1.2.1.min.js");
-$.getScript("https://alltay.github.io/blocks/slick.min.js"); // Need to change on relese
+$.getScript("https://alltay.github.io/blocks/slick.min.js"); // Need to change on release
 getSlider(cat_id, site_name)
 
 setTimeout(function(){
-    $.getScript("https://alltay.github.io/blocks/slider.js");// Need to change on relese
+    $.getScript("https://alltay.github.io/blocks/slider.js");// Need to change on release
 }, 2000);
 
+// Show widget with latensy
 setTimeout(function(){
-    document.getElementById("ad_sub_cat_widget").style.display = "block"; 
-}, 2000);
+    document.getElementById(widget).style.display = "block"; 
+}, 2500);
 
